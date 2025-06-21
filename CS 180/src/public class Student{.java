7c42046a356f56c4e5aa1a212f177422b2c475cb
Maninder Kaur <75804314+k4urman@users.noreply.github.com>
2025-06-21 19:38:18 -0400

    cs180 push

---
+++ CS 180/src/public class Student{.java
@@ -0,0 +1,13 @@
+public class Student{
+    public String name;
+
+
+    public Student(){
+        this.name="";
+    }
+    
+    public static void main (String[] args){
+        Student a = new Student();
+        a.name = "iman";
+    }
+}

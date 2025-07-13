bec25b15b0efc558b1be9f4694a5cc50b93a9863
Maninder Kaur <75804314+k4urman@users.noreply.github.com>
2025-07-13 17:04:28 -0400

    Create main.c

---
+++ SUMMER25/cs240/labs/lab2/v10/main.c
@@ -0,0 +1,20 @@
+// Basic string processing.
+
+#include <stdio.h>
+
+int main()
+{
+char x[10];
+int i;
+
+  x[0] = 'P';
+  x[1] = 'u';
+  x[2] = 'r';
+  x[3] = 'd';
+  x[4] = 'u';
+  x[5] = 'e';
+  x[6] = '\0';
+
+  printf("%s\n", x);
+
+}

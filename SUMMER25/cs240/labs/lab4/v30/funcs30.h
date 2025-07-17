b6d24ce55b7f02f7f9cad9347d741b6842121076
Maninder Kaur <75804314+k4urman@users.noreply.github.com>
2025-07-16 21:33:00 -0400

    Create funcs30.h

---
+++ SUMMER25/cs240/labs/lab4/v30/funcs30.h
@@ -0,0 +1,13 @@
+#ifndef FUNCS30_H
+#define FUNCS30_H
+
+#define MAX 18
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int inputproc(FILE **infile, FILE **outfile);
+void fileproc(FILE *infile, FILE *outfile, unsigned short ib);
+
+#endif

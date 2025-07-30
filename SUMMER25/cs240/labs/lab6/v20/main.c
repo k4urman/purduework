c3b4250ffc8b99b22355a9e3d149ee3176769bcb
Maninder Kaur <75804314+k4urman@users.noreply.github.com>
2025-07-30 18:55:10 -0400

    Update main.c

--- SUMMER25/cs240/labs/lab6/v20/main.c
+++ SUMMER25/cs240/labs/lab6/v20/main.c
@@ -1,1 +1,19 @@
+#include <stdio.h> //header file for all c files
 
+//function prototype for myconcat. Extern since it is
+//within the directory of main.c and c will find the other file
+extern char *myconcat(int n, ...);
+
+int main() {
+  // ati prints properly
+  printf("a + t + i= %s\n", myconcat(3, "a", "t", "i"));
+
+  // cat prints properly
+  printf("c + a + t = %s\n", myconcat(4, "c", "a", "t", "\0"));
+
+  //returns null because there are less arguments given then told
+  printf("c + a + t = %s\n", myconcat(4, "c", "a", "\0"));
+
+  // cat8 prints properly
+  printf("c + a + t + 8 = %s\n", myconcat(4, "c", "a", "t", "8"));
+}

0c79f3e76f70beb5c52b8428ec5954fbd6bf22fc
Maninder Kaur <75804314+k4urman@users.noreply.github.com>
2025-07-30 18:56:59 -0400

    Create funcs21.h

---
+++ SUMMER25/cs240/labs/lab6/v21/funcs21.h
@@ -0,0 +1,15 @@
+﻿﻿#ifndef FUNCS21_H
+#define FUNCS21_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <ctype.h>
+
+#define MAX_PROMPT 12
+#define MAX_CMD 256
+
+#endif
